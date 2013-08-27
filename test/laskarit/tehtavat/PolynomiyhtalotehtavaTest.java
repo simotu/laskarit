da74f18bcac0e82b9dcdd047c5af688dcfaa5f27
Simo Tukiainen <simo.tukiainen@helsinki.fi>
2013-08-27 09:59:35 +0300

    Laajennettu testien kattavuutta.

--- test/laskarit/tehtavat/PolynomiyhtalotehtavaTest.java
+++ test/laskarit/tehtavat/PolynomiyhtalotehtavaTest.java
@@ -19,6 +19,13 @@ public class PolynomiyhtalotehtavaTest {
             fail("Odotettiin virhettä.");
         }
         catch(IndexOutOfBoundsException e) {}
+        assertTrue("1 on yhtälön 0=0 juuri,", new Polynomiyhtalotehtava(0).onkoJuuri(1));
+        assertTrue("2 on yhtälön 0=0 juuri.", new Polynomiyhtalotehtava(0).onkoJuuri(2));
+        assertTrue("5 on yhtälön 0=0 juuri.", new Polynomiyhtalotehtava(0).onkoJuuri(5));
+        assertTrue("-10 on yhtälön 0=0 juuri", new Polynomiyhtalotehtava(0).onkoJuuri(-10));
+        assertFalse("2 ei ole yhtälön 1=0 juuri.", new Polynomiyhtalotehtava(1).onkoJuuri(2));
+        assertFalse("5 ei ole yhtälön 1=0 juuri.", new Polynomiyhtalotehtava(1).onkoJuuri(5));
+        assertFalse("0 ei ole yhtälön 1=0 juuri.", new Polynomiyhtalotehtava(1).onkoJuuri(0));
     }
 
     @Test
@@ -33,6 +40,10 @@ public class PolynomiyhtalotehtavaTest {
                 new Polynomiyhtalotehtava(1, 0, 0, 0, 0, 0).annaEsitys().toString(null));
         assertEquals("x^5+2x^2+x=0",
                 new Polynomiyhtalotehtava(1, 0, 0, 2, 1, 0).annaEsitys().toString(null));
+        assertEquals("x^2-4x-1=0",
+                new Polynomiyhtalotehtava(1, -4, -1).annaEsitys().toString(null));
+        assertEquals("-x^5=0",
+                new Polynomiyhtalotehtava(-1, 0, 0, 0, 0, 0).annaEsitys().toString(null));
     }
 
     /**
--- test/laskarit/tehtavat/YhteenlaskutehtavaTest.java
+++ test/laskarit/tehtavat/YhteenlaskutehtavaTest.java
@@ -15,8 +15,8 @@ public class YhteenlaskutehtavaTest {
 
     @Test
     public void testTarkistaVastaus() {
-        for(int i = 0; i < 100; i++) {
-            for(int j = 0; j < 100; j++) {
+        for(int i = -100; i < 100; i++) {
+            for(int j = -100; j < 100; j++) {
                 assertTrue(new Yhteenlaskutehtava(i, j).tarkistaVastaus("" + (i+j)));
                 assertFalse(new Yhteenlaskutehtava(i, j).tarkistaVastaus("" + (i+j+1)));
             }
